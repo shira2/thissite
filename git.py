3c36220a95c5ed50148e3c3afaf62ca8cbb11c2f
shira2 <pythonx20@gmail.com>
2020-08-22 19:24:53 +0300

    changes

--- git.py
+++ git.py
@@ -5,8 +5,12 @@ def hello_text():
     return "Hello World"
 
 
-print(hello_text())
+def bye_text():
+    return "good bye"
 
+
+print(hello_text())
+print(bye_text())
 
 '''
 print("my name is shir")
