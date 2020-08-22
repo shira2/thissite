e29697355ef2f477f3d7acbd9f6d741ee426afd7
shira2 <pythonx20@gmail.com>
2020-08-22 18:54:13 +0300

    initial commit

---
+++ git.py
@@ -0,0 +1,25 @@
+print("my name is shir")
+
+
+def hello_text():
+    return "Hello World"
+
+
+print(hello_text())
+
+
+'''
+print("my name is shir")
+
+
+def hello_text():
+    return "Hello World"
+
+
+def bye_text():
+    return "good bye"
+
+
+print(hello_text())
+print(bye_text())
+'''
